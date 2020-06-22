8f5de70d944aedd8bb6333200ef4eee4f8d9ad3b
César Magalhães <ceanma@gmail.com>
2020-06-22 08:55:49 -0300

    As chaves publica e privadas foram alteradas, além foi habilitado o "authorization_code"

--- oauth2-jwt-server/src/main/java/com/marcosbarbero/lab/sec/oauth/jwt/config/security/WebSecurityConfiguration.java
+++ oauth2-jwt-server/src/main/java/com/marcosbarbero/lab/sec/oauth/jwt/config/security/WebSecurityConfiguration.java
@@ -1,10 +1,13 @@
 package com.marcosbarbero.lab.sec.oauth.jwt.config.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -51,6 +54,29 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
             ((JdbcDaoImpl) userDetailsService).setDataSource(dataSource);
         }
         return userDetailsService;
+    }
+    
+//    @Override
+//	public void configure(HttpSecurity http) throws Exception {
+//		http.csrf().disable().exceptionHandling()
+//				.authenticationEntryPoint(
+//						(request, response, authException) -> response.sendError(HttpServletResponse.SC_UNAUTHORIZED))
+//				.and().authorizeRequests().antMatchers("/**").authenticated().and().httpBasic();
+//	}
+    
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http.authorizeRequests()
+        .anyRequest().authenticated()
+        .antMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+        .and()
+        .httpBasic()
+        .and()
+        .antMatcher("/**")
+        .sessionManagement()
+          .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        .and()
+        .csrf().disable();
     }
 
 }
--- oauth2-jwt-server/src/main/resources/data.sql
+++ oauth2-jwt-server/src/main/resources/data.sql
@@ -1,6 +1,6 @@
 -- The encrypted client_secret it `secret`
-INSERT INTO oauth_client_details (client_id, client_secret, scope, authorized_grant_types, authorities, access_token_validity)
-  VALUES ('clientId', '{bcrypt}$2a$10$vCXMWCn7fDZWOcLnIEhmK.74dvK1Eh8ae2WrWlhr2ETPLoxQctN4.', 'read,write', 'password,refresh_token,client_credentials', 'ROLE_CLIENT', 300);
+INSERT INTO oauth_client_details (client_id, client_secret, scope, authorized_grant_types, authorities, access_token_validity, refresh_token_validity, web_server_redirect_uri)
+  VALUES ('clientId', '{bcrypt}$2a$10$vCXMWCn7fDZWOcLnIEhmK.74dvK1Eh8ae2WrWlhr2ETPLoxQctN4.', 'read,write', 'client_credentials,password,refresh_token,implicit,authorization_code', 'ROLE_CLIENT', 300, 3600, 'http://localhost:8080/code');
 
 -- The encrypted password is `pass`
 INSERT INTO users (id, username, password, enabled) VALUES (1, 'user', '{bcrypt}$2a$10$cyf5NfobcruKQ8XGjUJkEegr9ZWFqaea6vjpXWEaSqTa2xL9wjgQC', 1);
